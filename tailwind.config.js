f2d3f69cc3416a9cc7e6f0af0cda71238a8d044e
nidhijadhav <nidhivjadhav@gmail.com>
2023-10-25 12:56:50 -0400

    responsive margins

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      spacing: {
+        '25%' : '25%',
+        '15%': '15%',
+        '10%': '10%',
+        '5%': '5%',
+        '3%': '3%',
+        '2%': '2%',
+        '1%': '1%'
+      },
       fontFamily: {
         abril: ["Abril Fatface", "serif"],
         gabarito: ["Gabarito", "sans-serif"],
